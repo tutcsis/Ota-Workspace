7bfd110c250aaf405a2f05820be7f62fa17fe087
kibaota <ohta.syunsuke@liveout.co.jp>
2025-11-10 10:23:45 +0900

    有害投稿数の月毎のグループのグラフを表示

---
+++ src/data_analyze/make_monthly_g_rate_graph.py
@@ -0,0 +1,73 @@
+from pathes import (
+	G_TWLEN_MONTHLY_TABLE_PATH,
+	G_YEARLY_TWLEN_GRAPH_PATH,
+	TOXIC_LABEL,
+	USE_YEARS,
+	GROUP
+)
+import utils
+import os
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tap import Tap
+from tqdm import tqdm
+from collections import Counter
+
+class Args(Tap):
+	table_path: str = G_TWLEN_MONTHLY_TABLE_PATH
+	graph_path: str = G_YEARLY_TWLEN_GRAPH_PATH
+	toxic_label: list = TOXIC_LABEL
+	years: list = USE_YEARS
+	groups: list = GROUP
+
+	# graph settings
+	graph_title: str = "Grouped Toxic Tweet Count"
+	graph_xlabel: str = "month"
+	graph_ylabel: str = "tweets"
+	colors = {
+		'0': 'darkorchid',
+		'1': 'royalblue',
+		'2': 'orangered',
+		'3': 'forestgreen',
+		'4': 'crimson',
+	}
+
+def main(args):
+	for toxic in args.toxic_label + ['all']:
+		table_file = os.path.join(args.table_path, f"{toxic}.csv")
+		graph_file = os.path.join(args.graph_path, f"{toxic}.png")
+		df = pd.read_csv(table_file, index_col=0)
+		df = df[df.index.str.split('-').str[0].isin(args.years)]
+		# 欠損月を0埋め
+		for year in args.years:
+			for month in range(1, 12+1):
+				id = f"{year}-{str(month).zfill(2)}"
+				if id not in df.index:
+					df.loc[id] = [0 for _ in range(len(df.columns))]
+					# print('Added id: ', id)
+		if not 'all' in df.columns:
+			df['all'] = df.sum(axis=1)
+		for group in args.groups:
+			df[str(group)] = df[str(group)]/df['all']*100
+		df = df.sort_index().drop('all', axis=1)
+		# print("id: ", list(df.index))
+		print(df)
+		plt.figure()
+		df.plot.bar(stacked=True)
+		plt.xticks(
+			range(0, 12*len(args.years)+1, 12),
+			args.years + [""]
+		)
+		plt.title(f"{args.graph_title} ({toxic})")
+		plt.xlabel(args.graph_xlabel)
+		plt.ylabel(args.graph_ylabel)
+		plt.tight_layout()
+		plt.savefig(graph_file)
+		plt.close()
+
+if __name__ == "__main__":
+	args = Args().parse_args()
+	main(args)
+
--- src/data_analyze/pathes.py
+++ src/data_analyze/pathes.py
@@ -10,12 +10,14 @@ G_USERS_TABLE_PATH = "/work/s245302/Ota-Workspace/tables/grouped_users/"
 YEARLY_TOXIC_USER_TABLE = "/work/s245302/Ota-Workspace/tables/toxic_users_yearly/"
 TOXIC_TW_COUNT_PATH = "/work/s245302/Ota-Workspace/tables/toxic_tw_count"
 G_TWLEN_TABLE_PATH = "/work/s245302/Ota-Workspace/tables/g_tw_counts_yearly/"
+G_TWLEN_MONTHLY_TABLE_PATH = "/work/s245302/Ota-Workspace/tables/g_tw_counts_monthly/"
 TWLEN_COUNT_TABLE_PATH = "/work/s245302/Ota-Workspace/tables/all_tw_counts_yearly/"
 ALL_TWLEN_TABLE_PATH = "/work/s245302/Ota-Workspace/tables/all_tw_len_counts.csv"
 
 # graph pathes
 G_TWLEN_GRAPH_PATH = "/work/s245302/Ota-Workspace/imgs/g_twcount_rate/"
 ALL_TWLEN_GRAPH_PATH = "/work/s245302/Ota-Workspace/imgs/all_tw_len_graph.png"
+G_YEARLY_TWLEN_GRAPH_PATH = "/work/s245302/Ota-Workspace/imgs/g_twcount_rate_yearly/"
 
 TOXIC_LABEL = ["obscene", "discriminatory", "violent"]
 USE_YEARS = ["2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"]
